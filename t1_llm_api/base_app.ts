63fb0e8457c93346e69b08dd40bde5f2ed2106f2
Narek Hovhannisyan <narek_hovhannisyan1@epam.com>
2026-08-04 20:44:33 +0400

    Implement base app.

--- t1_llm_api/base_app.ts
+++ t1_llm_api/base_app.ts
@@ -3,7 +3,6 @@ import * as readline from "node:readline/promises";
 import { Conversation, Message, Role } from "../commons";
 import AIClient from "./base_client";
 
-
 /**
  * Start an interactive chat session with an AI client.
  *
@@ -20,13 +19,30 @@ import AIClient from "./base_client";
  * @param client The AI client instance to use for generating responses.
  */
 export async function start(stream: boolean, client: AIClient) {
-  //TODO:
-  // - Create a Conversation instance
-  // - Set up a readline interface for user input
-  // - Loop until user types 'exit':
-  //     - Prompt the user for input
-  //     - Add a USER Message to the conversation
-  //     - Call client.streamResponse or client.response based on `stream`
-  //     - Add the AI response Message to the conversation
-  throw new Error("Not implemented.");
+  const conversation = new Conversation();
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  console.log("Type your question or 'exit' to quit.");
+
+  while (true) {
+    const input = await rl.question("➡️. ");
+
+    if (input === "exit") {
+      console.log("Exiting the chat. Goodby!");
+      rl.close();
+      process.exit(0);
+    }
+
+    conversation.addMessage(new Message(Role.USER, input));
+    process.stdout.write("🤖: ");
+
+    let aiMessage: Message = stream
+      ? await client.streamResponse(conversation.messages)
+      : await client.response(conversation.messages);
+
+    conversation.addMessage(aiMessage);
+  }
 }
