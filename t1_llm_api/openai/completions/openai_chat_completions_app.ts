598d4034913e5af10dd446aa8e9db2a755994193
Narek Hovhannisyan <narek_hovhannisyan1@epam.com>
2026-08-05 06:27:53 +0400

    Implement openai chat completion app

--- t1_llm_api/openai/completions/openai_chat_completions_app.ts
+++ t1_llm_api/openai/completions/openai_chat_completions_app.ts
@@ -1,21 +1,25 @@
 import { OpenAIClient } from "./client";
 import { CustomOpenAIClient } from "./custom_client";
 
-import { OPENAI_API_KEY, OPENAI_CHAT_COMPLETIONS_ENDPOINT, DEFAULT_SYSTEM_PROMPT } from "../../../commons";
+import {
+  OPENAI_API_KEY,
+  OPENAI_CHAT_COMPLETIONS_ENDPOINT,
+  DEFAULT_SYSTEM_PROMPT,
+} from "../../../commons";
 import { start } from "../../base_app";
 
 const openAIClient = new OpenAIClient(
   OPENAI_CHAT_COMPLETIONS_ENDPOINT,
-  'gpt-5.2',
+  "gpt-5.4-nano",
   OPENAI_API_KEY,
   DEFAULT_SYSTEM_PROMPT,
 );
 
 const openAICustomClient = new CustomOpenAIClient(
   OPENAI_CHAT_COMPLETIONS_ENDPOINT,
-  'gpt-5.2',
+  "gpt-5.4-nano",
   OPENAI_API_KEY,
   DEFAULT_SYSTEM_PROMPT,
 );
 
-start(true, openAIClient);
+start(false, openAIClient);
